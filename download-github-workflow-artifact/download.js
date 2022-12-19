080cd2c7deb508fad607382ae4c3850046e0d25e
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2022-12-19 10:46:44 -0500

    Add workflow artifact download (#26)
    * Added artifact download action
    
    * Fix typo
    
    * Fix input
    
    * Fix bash bug
    
    * Added zip suffix
    
    * Fix log messages
    
    * Added readme
    
    * Try new default for download_dir
    
    * Remove step not needed and add more verbose logging message during download
    
    * Update download-github-workflow-artifact/action.yml
    
    Co-authored-by: Alberto Fumagalli <alberto@xanadu.ai>
    
    * Update linear backoff to exponential with a capped ceiling
    
    * Removed default for maxRetry in JS script
    
    Co-authored-by: Alberto Fumagalli <alberto@xanadu.ai>

---
+++ download-github-workflow-artifact/download.js
@@ -0,0 +1,58 @@
+let fs = require('fs');
+
+module.exports = async ({github, context}, workflow_run_id, artifact_name_regex, artifact_download_dir, max_retry) => {
+    // Exponential backoff with a ceiling at 30 seconds
+    const backoffDelay = (retryCount) => new Promise(resolve => setTimeout(resolve, 100 * Math.min(300, 1 << retryCount)));
+
+    const downloadArtifact = async (artifact) => {
+        let download = await github.rest.actions.downloadArtifact({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            artifact_id: artifact.id,
+            archive_format: 'zip'
+        });
+        fs.writeFileSync(`${artifact_download_dir}/${artifact.name}.zip`, Buffer.from(download.data));
+    };
+
+    let artifactsAll = await github.rest.actions.listWorkflowRunArtifacts({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        run_id: workflow_run_id
+    });
+    let artifactsFiltered = artifactsAll.data.artifacts.filter((artifact) => {
+        return artifact.name.match(artifact_name_regex)
+    });
+
+    /*
+      Attempt to download the artifact one at a time and save them to disk.
+      In the event of an error, back-off (incrementally) and try again.
+
+      Each error increases the back-off delay by 1s, in other words:
+      > failure -> wait (1s) -> try-again -> failure -> wait (2s) -> try-again -> ...
+
+      This deals with network congestion and rate-limiting errors that occur if the artifact
+      download happens during a time of high load on GitHub Actions.
+    */
+    for (const artifact of artifactsFiltered) {
+        for (let i = 1; i < max_retry + 1; i++) {
+            const artifactFullName = `${artifact_download_dir}/${artifact.name}.zip`;
+            try {
+                console.log(`Attempting to download artifact (${artifact.id}): ${artifactFullName}`);
+                await downloadArtifact(artifact);
+                console.log(`Successfully downloaded artifact (${artifact.id}): ${artifactFullName}`);
+                break;
+            } catch (e) {
+                console.log(`Error while trying to download artifact (${artifact.id}): ${artifactFullName}, retryCount: ${i}, error: ${e}`);
+                ['message', 'status', 'request', 'response'].forEach((attr) => {
+                    if (e.hasOwnProperty(attr)) {
+                        console.log(`error_${attr}:`);
+                        console.log(e[attr]);
+                    }
+                });
+                if (i === max_retry) throw new Error(e);
+                console.log(`Retrying download of artifact (${artifact.id}): ${artifactFullName}`);
+                await backoffDelay(i);
+            }
+        }
+    }
+}
