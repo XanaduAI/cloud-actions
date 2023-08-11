4e99033b6beced29c88c0e1cd58f6b97f1aecee5
martellato <m_zamani@live.com>
2023-08-11 16:47:27 -0400

    fixes

--- og/lint/resolve.py
+++ og/lint/resolve.py
@@ -4,24 +4,29 @@ from pathlib import Path
 
 ignore_files = set()
 
-if (ignore_file := Path("./.lintignore")).isfile():
+if (ignore_file := Path("./.lintignore")).is_file():
     ignore_paths = ignore_file.read_text().split()
     for path in ignore_paths:
-        ignore_files.update(path.rglob("*"))
+        path = Path(path)
+        if path.is_dir():
+            ignore_files.update(map(Path.resolve, path.rglob("*")))
+        else:
+            ignore_files.update([path.resolve()])
 
 edited_py_files = {
     _file
-    for file in subprocess(
+    for file in subprocess.run(
         f"git diff origin/{os.environ['BASE_BRANCH']} --diff-filter=MAC --name-only",
         shell=True,
         stdout=subprocess.PIPE,
+        encoding="utf-8",
     ).stdout.split()
     if (_file := Path(file).resolve()).suffix == ".py"
 }
 
 files_to_format = edited_py_files - ignore_files
 
-if files_to_format := " ".join(files_to_format):
+if files_to_format := " ".join(map(str, files_to_format)):
     print("Running black...")
     subprocess.run(f"black {files_to_format}", shell=True)
     print("Running isort...")
@@ -31,7 +36,7 @@ if files_to_format := " ".join(files_to_format):
         subprocess.run(
             f"docformatter --recursive --in-place --wrap-summaries 88 --wrap-descriptions 88 {files_to_format}",
             shell=True,
-        ).rc
+        ).returncode
         == 3
     ):
         print("Docformatter raised status 3")
