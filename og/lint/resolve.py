b7f03e9efc716273938843369cdd273c4acb9cdd
martellato <35515345+martellato@users.noreply.github.com>
2023-08-17 15:08:48 -0400

    Allow creation of `.lintignore` file to ignore linting of files and directories (#47)
    * global ignores
    
    * fixes

---
+++ og/lint/resolve.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+from pathlib import Path
+
+ignore_files = set()
+
+if (ignore_file := Path("./.lintignore")).is_file():
+    ignore_paths = ignore_file.read_text().split()
+    for path in ignore_paths:
+        path = Path(path)
+        if path.is_dir():
+            ignore_files.update(map(Path.resolve, path.rglob("*")))
+        else:
+            ignore_files.update([path.resolve()])
+
+edited_py_files = {
+    _file
+    for file in subprocess.run(
+        f"git diff origin/{os.environ['BASE_BRANCH']} --diff-filter=MAC --name-only",
+        shell=True,
+        stdout=subprocess.PIPE,
+        encoding="utf-8",
+    ).stdout.split()
+    if (_file := Path(file).resolve()).suffix == ".py"
+}
+
+files_to_format = edited_py_files - ignore_files
+
+if files_to_format := " ".join(map(str, files_to_format)):
+    print("Running black...")
+    subprocess.run(f"black {files_to_format}", shell=True)
+    print("Running isort...")
+    subprocess.run(f"isort --profile=black {files_to_format}", shell=True)
+    print("Running docformatter")
+    if (
+        subprocess.run(
+            f"docformatter --recursive --in-place --wrap-summaries 88 --wrap-descriptions 88 {files_to_format}",
+            shell=True,
+        ).returncode
+        == 3
+    ):
+        print("Docformatter raised status 3")
+    print("Running flake8")
+    subprocess.run(
+        f"flake8 --ignore=E203,E501,W503 {files_to_format}", shell=True, check=True
+    )
