6c8dfd026ba2e6fbb84f553754e223cbc3432503
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-10-23 18:21:20 -0400

    Updating download artifact action

--- download-github-workflow-artifact/download.js
+++ download-github-workflow-artifact/download.js
@@ -1,13 +1,13 @@
 let fs = require('fs');
 
-module.exports = async ({github, context}, workflow_run_id, artifact_name_regex, artifact_download_dir, max_retry) => {
+module.exports = async ({github}, owner, repo, workflow_run_id, artifact_name_regex, artifact_download_dir, max_retry) => {
     // Exponential backoff with a ceiling at 30 seconds
     const backoffDelay = (retryCount) => new Promise(resolve => setTimeout(resolve, 100 * Math.min(300, 1 << retryCount)));
 
     const downloadArtifact = async (artifact) => {
         let download = await github.rest.actions.downloadArtifact({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
+            owner: owner,
+            repo: repo,
             artifact_id: artifact.id,
             archive_format: 'zip'
         });
@@ -15,8 +15,8 @@ module.exports = async ({github, context}, workflow_run_id, artifact_name_regex,
     };
 
     let artifactsAllOpts = github.rest.actions.listWorkflowRunArtifacts.endpoint.merge({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
+        owner: owner,
+        repo: repo,
         run_id: workflow_run_id
     });
     let artifactsAll = await github.paginate(artifactsAllOpts);
