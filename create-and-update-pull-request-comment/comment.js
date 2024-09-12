321bc376bdb5e857475246207366f3401467ebd0
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-09-12 11:39:56 -0400

    Update script to not paginate existing comments if a commentUid is not provided

--- create-and-update-pull-request-comment/comment.js
+++ create-and-update-pull-request-comment/comment.js
@@ -7,29 +7,33 @@ module.exports = async ({github, context}, prNumber, commentUid, commentBody) =>
     const commentContent = `${commentBody}\n${commentHeader}`;
     console.log(commentContent);
 
-    const opts = github.rest.issues.listComments.endpoint.merge({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: prNumber
-    });
-    const comments = await github.paginate(opts);
+    if (commentUid) {
+      const opts = github.rest.issues.listComments.endpoint.merge({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          issue_number: prNumber
+      });
+      const comments = await github.paginate(opts);
 
-    // Find any comment already made by the bot.
-    const botComment = comments.find(comment => comment.user.id === actionsBotUserId && comment.body.trim().endsWith(commentHeader));
+      // Find any comment already made by the bot.
+      const botComment = comments.find(comment => comment.user.id === actionsBotUserId && comment.body.trim().endsWith(commentHeader));
 
-    if (botComment && commentUid) {
+      if (botComment) {
         await github.rest.issues.updateComment({
             owner: context.repo.owner,
             repo: context.repo.repo,
             comment_id: botComment.id,
             body: commentContent
         });
-    } else {
-        await github.rest.issues.createComment({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            issue_number: prNumber,
-            body: commentContent
-        });
+
+        return;
+      }
     }
+
+    await github.rest.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: prNumber,
+        body: commentContent
+    });
 }
